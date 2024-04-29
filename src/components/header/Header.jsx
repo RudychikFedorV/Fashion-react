76ec9589f710c5eb19aa67e296e49e36f9f93939
RudychikFedorV <rudychik.fedor@gmail.com>
2024-04-29 20:47:27 +0300

    header, project preparation

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logoImg from './../../img/icons/logo.svg';
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header-row">
+          <div className="header-logo">
+            <img src={logoImg} alt="Logo" />
+            <span>FASHION</span>
+          </div>
+          <nav className="header-nav">
+            <ul>
+                <li><a href="#!">CATALOGUE</a></li>
+                <li><a href="#!">FASHION</a></li>
+                <li><a href="#!">FAVOURITE</a></li>
+                <li><a href="#!">LIFESTYLE</a></li>
+                <li><a href="#!" className="header-nav__btn">SIGN UP</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
